965a5e10bd6264bdd90e4d51cb90697e4fe38674
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2024-02-29 13:19:02 -0300

    merge resources that belongs to the ec2 service

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -37,3 +37,61 @@ resource "aws_launch_template" "launch_template" {
 
   user_data = filebase64("${path.module}/ecs.sh")
 }
+
+resource "aws_autoscaling_group" "ecs_asg" {
+  name             = "infra-ecs-cluster-aluraflix"
+  desired_capacity = 2
+  max_size         = 3
+  min_size         = 1
+
+  launch_template {
+    id      = aws_launch_template.launch_template.id
+    version = "$Latest"
+  }
+
+  vpc_zone_identifier = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+
+  tag {
+    key                 = "AmazonECSManaged"
+    value               = true
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "ecs_alb" {
+  name               = "ecs-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.security_group.id]
+  subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+
+  enable_deletion_protection = true
+
+  tags = {
+    Name = "ecs-alb"
+  }
+}
+
+resource "aws_lb_target_group" "ecs_tg" {
+  name        = "ecs-target-group"
+  port        = "80"
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+
+  health_check {
+    path = "/"
+  }
+}
+
+resource "aws_lb_listener" "ecs_alb_listener" {
+  load_balancer_arn = aws_lb.ecs_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_tg.arn
+  }
+}
+

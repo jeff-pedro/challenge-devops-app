d64d9d0a858327065f48fb5fe13bab2bd3692628
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2024-03-01 14:54:32 -0300

    move module Dev to Prod

--- env/prod/main.tf
+++ env/prod/main.tf
@@ -1,9 +1,9 @@
-module "Dev" {
+module "Prod" {
   source = "../../infra"
 
   name          = "aluraflix"
   image_name    = "aluraflix-api"
   image_version = "latest"
   vpc_cidr      = "10.0.0.0/16"
-  key           = "ecs-dev"
+  key           = "ecs-prod"
 }

b7eccdbcaca618e0ee64c1b763716b63e195dfbf
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2024-03-05 17:09:06 -0300

    change capacity provider params

--- infra/ecs/main.tf
+++ infra/ecs/main.tf
@@ -20,10 +20,10 @@ resource "aws_ecs_capacity_provider" "capacity_provider" {
     auto_scaling_group_arn = var.asg_arn
 
     managed_scaling {
-      maximum_scaling_step_size = 1000
+      maximum_scaling_step_size = 10
       minimum_scaling_step_size = 1
       status                    = "ENABLED"
-      target_capacity           = 2
+      target_capacity           = 90
     }
   }
 }
